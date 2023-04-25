f189af91f3c40f8756e044441ea8d49c430d6088
Orest278 <125860863+Orest278@users.noreply.github.com>
2023-04-25 12:08:52 +0300

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(`.login-form`);
 
+loginForm.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+
+    const formData = {};
+    const formElements = event.target.elements;
+
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+
+        if (element.tagName === `INPUT`) { 
+            const name = element.getAttribute(`name`);
+            const value = element.value.trim();
+
+            if (!value) {
+                alert('Всі поля повинні бути заповнені!');
+                return
+            }
+
+            formData[name] = value;
+        }
+    }
+
+    console.log(formData);
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector(`body`);
+const changeColor = document.querySelector(`.change-color`);
+const colorEl = document.querySelector(`.color`);
+
+changeColor.addEventListener(`click`, () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorEl.textContent = randomColor;
+});
